22cbb84fc86543051d44e3880c54edd0b8f436ce
Tyler Pollak <tpollak@ucsd.edu>
2016-09-25 14:05:30 -0500

    First Commit - From existing repo

---
+++ BenchCoachBS.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import csv
+import urllib, urllib2
+
+# r = urllib.urlopen('http://www.baseballpress.com/lineups').read()
+# soup = BeautifulSoup(r, 'html.parser')
+# for link in soup.find_all('a'):
+# 	print(link.get('href'))
+
+# Helper function for text
+def send_text(numstring, message):
+    data = urllib.urlencode({'number': numstring, 'message': message})
+    r = urllib2.urlopen("http://textbelt.com/text", data)
+    return r.getcode(), r.read()
+
+#Open your roster
+f = open('roster.csv', 'rbU')
+IDlist = []
+
+try:
+	reader = csv.reader(f)
+	for row in reader:
+		IDlist.append([row[0], row[1], ""])
+finally:
+	f.close()
+
+
+url = 'http://crunchtimebaseball.com/master.csv'
+response = urllib.urlopen(url)
+MLBlist = csv.reader(response)
+
+
+for row in MLBlist:
+	for x in range(0, len(IDlist)):
+		if row[1] in IDlist[x]:
+			print IDlist[x][0]
+
+
+
+#print send_text(8182812111, "lasttest")
+
+
+
+
+# for row in MLBlist:
+# 	if 'Andrew Benintendi' in row:
+# 		print "mlb_id is: " + row[0] + " and mlb_name is: " + row[1]
+
+
+
+
+
+# Use CSV file to get the mlb_id from your team's players, then go to baseballpress lineup to see if that mlb_id is somewhere in the page.  if not, then send text
